564e98ca518f944cebc16ae44dea7dbb82e8a3ff
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-11-30 16:48:05 +0100

    add std::chrono::duration parsing

--- include/utils/parse_string.h
+++ include/utils/parse_string.h
@@ -1,9 +1,12 @@
 #ifndef __PARSE_STRING_H__
 #define __PARSE_STRING_H__
 
+#include <fmt/format.h>
+
 #include <string>
 #include <charconv>
 #include <optional>
+#include <chrono>
 
 template<typename T> struct string_parser;
 
@@ -26,6 +29,30 @@ template<> struct string_parser<bool> {
         } else {
             return std::nullopt;
         }
+    }
+};
+
+template<typename Rep, typename Period>
+struct string_parser<std::chrono::duration<Rep, Period>> {
+    std::optional<std::chrono::duration<Rep, Period>> operator()(std::string_view str) {
+        if (auto value = string_parser<Rep>{}(str)) {
+            return std::chrono::duration<Rep, Period>{*value};
+        } else {
+            return std::nullopt;
+        }
+    }
+};
+
+template<typename Rep, typename Period>
+struct fmt::formatter<std::chrono::duration<Rep, Period>> {
+    template<typename Context>
+    constexpr auto parse(Context &ctx) {
+        return ctx.begin();
+    }
+
+    template<typename Context>
+    auto format(const std::chrono::duration<Rep, Period> &value, Context &ctx) {
+        return fmt::format_to(ctx.out(), "{}", value.count());
     }
 };
 
