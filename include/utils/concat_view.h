6ac28c5294801eeb4c68f070b71c8c9c6d35b12e
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-12-11 17:54:55 +0100

    improve concat_view

--- include/utils/concat_view.h
+++ include/utils/concat_view.h
@@ -6,6 +6,11 @@
 #include <tuple>
 
 namespace util {
+
+    template<typename ... Ts> using common_iterator_concept = 
+        std::conditional_t<(std::bidirectional_iterator<Ts> && ...),
+            std::bidirectional_iterator_tag,
+            std::forward_iterator_tag>;
     
     template <size_t I, typename T> struct indexed {
         T value;
@@ -25,15 +30,16 @@ namespace util {
 
     template<typename ... Ts> using index_variant_t = typename index_variant<Ts...>::type;
 
-    template<typename ... Ts> class concat_view;
+    template<std::ranges::forward_range ... Ts> class concat_view;
+
+    template<typename T> concept has_bidirectional_tag = std::derived_from<typename T::iterator_concept, std::bidirectional_iterator_tag>;
 
     template<typename ... Ts>
     class concat_view_iterator {
     public:
-        using iterator_variant = index_variant_t<std::ranges::iterator_t<Ts> ...>;
-        using iterator_category = std::input_iterator_tag;
+        using iterator_concept = common_iterator_concept<std::ranges::iterator_t<Ts> ...>;
         using value_type = std::common_type_t<std::ranges::range_value_t<Ts>...>;
-        using difference_type = std::ptrdiff_t;
+        using difference_type = std::common_type_t<std::ranges::range_difference_t<Ts> ...>;
 
         explicit concat_view_iterator() = default;
 
@@ -42,37 +48,33 @@ namespace util {
             : m_value(std::forward<Us>(arg) ...)
             , m_view(view) {}
         
-        bool operator==(const concat_view_iterator &other) const noexcept = default;
+        bool operator==(const concat_view_iterator &other) const = default;
 
         inline concat_view_iterator &operator++();
-        void operator++(int) {
-            auto copy = *this;
-            ++(*this);
-            return copy;
-        }
+        concat_view_iterator operator++(int) { auto copy = *this; ++(*this); return copy; }
 
-        const value_type &operator*() const {
-            return std::visit<const value_type &>([](const auto &it) -> decltype(auto) {
+        inline concat_view_iterator &operator--() requires has_bidirectional_tag<concat_view_iterator<Ts...>>;
+        concat_view_iterator operator--(int) requires std::derived_from<iterator_concept, std::bidirectional_iterator_tag> { auto copy = *this; --(*this); return copy; }
+
+        value_type operator*() const {
+            return std::visit<value_type>([](const auto &it) -> decltype(auto) {
                 return *it.value;
             }, m_value);
         }
-        inline const value_type *operator->() const {
-            return &(*this);
-        }
     
     private:
-        iterator_variant m_value;
+        index_variant_t<std::ranges::iterator_t<Ts> ...> m_value;
         concat_view<Ts ...> *m_view = nullptr;
     };
 
-    template<typename ... Ts>
-    class concat_view {
+    template<std::ranges::forward_range ... Ts>
+    class concat_view : std::ranges::view_interface<concat_view<Ts ... >> {
     public:
         using iterator = concat_view_iterator<Ts...>;
 
         template<typename ... Us>
         concat_view(Us && ... ranges)
-            : m_value{std::forward<Us>(ranges) ... } {}
+            : m_ranges{std::forward<Us>(ranges) ... } {}
         
         auto begin() {
             if (!m_begin) m_begin = find_begin<0>();
@@ -81,17 +83,25 @@ namespace util {
 
         auto end() {
             static constexpr auto last = sizeof...(Ts) - 1;
-            return iterator(this, std::in_place_index<last>, std::ranges::end(std::get<last>(m_value)));
+            return iterator(this, std::in_place_index<last>, std::ranges::end(std::get<last>(m_ranges)));
+        }
+
+        auto rbegin() requires std::bidirectional_iterator<iterator> {
+            return std::reverse_iterator(end());
+        }
+
+        auto rend() requires std::bidirectional_iterator<iterator> {
+            return std::reverse_iterator(begin());
         }
 
     private:
-        std::tuple<Ts ...> m_value;
+        std::tuple<Ts ...> m_ranges;
 
         std::optional<iterator> m_begin;
 
         template<size_t I>
         iterator find_begin() {
-            auto &range = std::get<I>(m_value);
+            auto &range = std::get<I>(m_ranges);
             auto it = std::ranges::begin(range);
             if constexpr (I == sizeof...(Ts) - 1) {
                 return iterator(this, std::in_place_index<I>, std::move(it));
@@ -99,6 +109,21 @@ namespace util {
                 return iterator(this, std::in_place_index<I>, std::move(it));
             } else {
                 return find_begin<I + 1>();
+            }
+        }
+
+        template<size_t I>
+        iterator find_last() {
+            auto &range = std::get<I>(m_ranges);
+            auto it = std::ranges::end(range);
+            if constexpr (I == 0) {
+                --it;
+                return iterator(this, std::in_place_index<I>, std::move(it));
+            } else if (it != std::ranges::begin(range)) {
+                --it;
+                return iterator(this, std::in_place_index<I>, std::move(it));
+            } else {
+                return find_last<I - 1>();
             }
         }
 
@@ -110,7 +135,7 @@ namespace util {
         std::visit([&]<size_t I, typename T>(indexed<I, T> &it){
             ++it.value;
             if constexpr (I != sizeof...(Ts) - 1) {
-                if (it.value == std::ranges::end(std::get<I>(m_view->m_value))) {
+                if (it.value == std::ranges::end(std::get<I>(m_view->m_ranges))) {
                     *this = m_view->template find_begin<I + 1>();
                 }
             }
@@ -118,7 +143,23 @@ namespace util {
         return *this;
     }
 
-    template<typename ... Ts> concat_view(Ts && ...) -> concat_view<Ts ...>;
+    template<typename ... Ts>
+    inline concat_view_iterator<Ts...> &concat_view_iterator<Ts...>::operator--()
+    requires has_bidirectional_tag<concat_view_iterator<Ts...>>
+    {
+        std::visit([&]<size_t I, typename T>(indexed<I, T> &it){
+            if constexpr (I != 0) {
+                if (it.value == std::ranges::begin(std::get<I>(m_view->m_ranges))) {
+                    *this = m_view->template find_last<I - 1>();
+                    return;
+                }
+            }
+            --it.value;
+        }, m_value);
+        return *this;
+    }
+
+    template<std::ranges::forward_range ... Ts> concat_view(Ts && ...) -> concat_view<Ts ...>;
 
 }
 
