2a698affc93c3c8628c076f5b68afaaeaa67c3b0
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-11-09 20:02:41 +0100

    cleanup enum_format

--- include/utils/enum_format.h
+++ include/utils/enum_format.h
@@ -7,15 +7,10 @@
 #include "parse_string.h"
 
 template<enums::enum_with_names E>
-struct fmt::formatter<E> {
-    template<typename Context>
-    constexpr auto parse(Context &ctx) {
-        return ctx.begin();
-    }
-
+struct fmt::formatter<E> : fmt::formatter<std::string_view> {
     template<typename Context>
     auto format(E value, Context &ctx) {
-        return fmt::format_to(ctx.out(), "{}", ::enums::to_string(value));
+        return fmt::formatter<std::string_view>::format(::enums::to_string(value), ctx);
     }
 };
 
